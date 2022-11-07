b06a1f30c098ba8c9c7bfab98cb95d4cd1916828
Sterlios <kuzmin.s.anton@gmail.com>
2022-11-07 22:30:14 +0100

    Initial commit

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+namespace TelegramBot
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //СompetitionBot bot = new СompetitionBot("5669645963:AAEsExKlzrvMQf0vQ5UhmkDDPtDNpoyUgfM");//NaturinoTestBot
+            СompetitionBot bot = new СompetitionBot("5700546220:AAH4DMxllrI55rR_tm4wC00a6w12Jei2U3o");//TestBot
+            bot.Start();
+        }
+
+    }
+}
---
+++ TelegramBot/СompetitionBot.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using System.Collections.Generic;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot
+{
+    class СompetitionBot
+    {
+        
+        private ITelegramBotClient _bot;
+        private List<ChatId> _moderators = new List<ChatId>();
+        private Chat _channel;
+        private const string _applyButton = "apply";
+        private const string _cancelButton = "cancel";
+
+        public СompetitionBot(string token)
+        {
+            _bot = new TelegramBotClient(token);
+            //_moderators.Add(474698824); // @alinavolynets
+            _moderators.Add(737444990); // @Kuzmin_Anton_S
+            _channel = _bot.GetChatAsync("@TestForBotNa").Result;
+            //_channel = _bot.GetChatAsync("@testcontestexample").Result;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Запущен бот " + _bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+
+            _bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+
+            Console.ReadLine();
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await HandleMassage(update);
+                return;
+            }
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
+            {
+                await HandleButton(update);
+                return;
+            }
+        }
+
+        private async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        private async Task HandleMassage(Update update)
+        {
+            if (update.Message.Type == MessageType.Text ||
+                update.Message.Type == MessageType.Video || 
+                update.Message.Type == MessageType.VideoNote)
+            {
+                foreach (var moderator in _moderators)
+                {
+                    await SendToModerator(moderator, update);
+                }
+
+                await _bot.SendTextMessageAsync(update.Message.Chat, "Сообщение принято на модерацию!");
+            }
+        }
+
+        private async Task SendToModerator(ChatId moderator, Update update)
+        {
+            Message memberMessage = _bot.ForwardMessageAsync(moderator, update.Message.Chat, update.Message.MessageId).Result;
+
+            if (update.Message.Type == MessageType.Video || update.Message.Type == MessageType.VideoNote)
+            {
+                var buttons = new InlineKeyboardMarkup(new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Принять", _applyButton),
+                    },
+                    new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData("Отклонить", _cancelButton),
+                    },
+                });
+
+                Message buttonCallBack = _bot.SendTextMessageAsync(moderator,
+                    $"Видео от пользователя " +
+                    $"{update.Message.Chat.FirstName} " +
+                    $"{update.Message.Chat.LastName} " +
+                    $"(@{update.Message.Chat.Username})",
+                    replyMarkup: buttons, replyToMessageId: memberMessage.MessageId).Result;
+            }
+        }
+
+        private async Task HandleButton(Update update)
+        {
+            var pressedButtonID = update.CallbackQuery.Data;
+            var messageId = update.CallbackQuery.Message.MessageId - 1;
+            //var fromMember = update.
+
+            switch (pressedButtonID)
+            {
+                case _applyButton:
+                    await _bot.ForwardMessageAsync(_channel, update.CallbackQuery.Message.Chat, update.CallbackQuery.Message.ReplyToMessage.MessageId);
+                    await _bot.SendTextMessageAsync(update.CallbackQuery.Message.Chat,
+                        $"Видео от пользователя " +
+                        $"{update.CallbackQuery.Message.Chat.FirstName} " +
+                        $"{update.CallbackQuery.Message.Chat.LastName} " +
+                        $"(@{update.CallbackQuery.Message.Chat.Username})\n" +
+                        $"ПРИНЯТО",
+                        replyToMessageId: update.CallbackQuery.Message.MessageId - 1);
+                    await _bot.DeleteMessageAsync(update.CallbackQuery.Message.Chat, update.CallbackQuery.Message.MessageId);
+                    break;
+                case _cancelButton:
+                    await _bot.SendTextMessageAsync(update.CallbackQuery.Message.Chat,
+                        $"Видео от пользователя " +
+                        $"{update.CallbackQuery.Message.Chat.FirstName} " +
+                        $"{update.CallbackQuery.Message.Chat.LastName} " +
+                        $"(@{update.CallbackQuery.Message.Chat.Username})\n" +
+                        $"ОТКЛОНЕНО",
+                        replyToMessageId: update.CallbackQuery.Message.MessageId - 1);
+                    await _bot.DeleteMessageAsync(update.CallbackQuery.Message.Chat, update.CallbackQuery.Message.MessageId);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
