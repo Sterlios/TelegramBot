250f8c85178b7f594ee1a1e035d3dfebe880e919
Sterlios <kuzmin.s.anton@gmail.com>
2022-11-09 11:56:10 +0100

    /

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -14,8 +14,12 @@ namespace TelegramBot
         static void Main(string[] args)
         {
             //СompetitionBot bot = new СompetitionBot("5669645963:AAEsExKlzrvMQf0vQ5UhmkDDPtDNpoyUgfM");//NaturinoTestBot
+<<<<<<< HEAD
             //СompetitionBot bot = new СompetitionBot("5505851101:AAFWvcFWi-fgrNzyTm0geJbdWHxtYuhydeY");//Naturino_New_Bot
             ContestBot bot = new ContestBot("5700546220:AAH4DMxllrI55rR_tm4wC00a6w12Jei2U3o");//TestBot
+=======
+            СompetitionBot bot = new СompetitionBot("5700546220:AAH4DMxllrI55rR_tm4wC00a6w12Jei2U3o");//TestBot
+>>>>>>> parent of 83db804 (FixEmptyMessage)
             bot.Start();
         }
 
